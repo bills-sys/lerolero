f0e653563a0441962a31d2bd198781ff42b3376f
bills-sys <85712144+bills-sys@users.noreply.github.com>
2021-09-07 19:08:39 -0300

    Commit commited

---
+++ src/main/java/com/billssys/lerolero/LeroleroApplication.java
@@ -0,0 +1,13 @@
+package com.billssys.lerolero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeroleroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeroleroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/billssys/lerolero/LeroleroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billssys.lerolero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeroleroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
